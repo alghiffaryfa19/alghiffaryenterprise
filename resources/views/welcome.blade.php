5e11094c5e00fffef8b224f15d02fe397f558318
Fauzan <alghiffaryfa19@gmail.com>
2019-11-11 21:29:19 +0700

    upload alghiffaryenterprise

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Al Ghiffary Enterprise</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/--mp---navbar-shrinking-on-scroll.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Card-Deck.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Card-Group-Classic.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Contact-form.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/dh-highlight-icon-heading.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/divider-text-middle.cs')}}s">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Paralax-Hero-Banner.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/styles.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Swiper-Slider-Card-For-Blog-Or-Product-1.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Swiper-Slider-Card-For-Blog-Or-Product.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Video-Parallax-Background-v2.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Pretty-Footer.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Pretty-Registration-Form.css')}}">
+    <link rel="stylesheet" href="{{ asset('alghiffaryenterprise/css/Paralax-Hero-Banner-1.css')}}">
+</head>
+<style>
+    .html {
+  scroll-behavior: smooth;
+}
+.box{
+    padding:60px 0px;
+}
+
+.box-part{
+    background:#FFF;
+    border-radius:0;
+    padding:60px 10px;
+    margin:30px 0px;
+}
+.text{
+    margin:20px 0px;
+}
+
+.fa{
+     color:#4183D7;
+}
+</style>
+<body>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top bg-dark">
+        <div class="container-fluid"><a class="navbar-brand" href="#"><img id="brand-logo" src="{{ asset('alghiffaryenterprise/alghiffaryfa.png')}}"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active text-white" href="#home">Beranda</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-white active" href="#lb">Tentang</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-light active" href="#time">Waktu</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-light active" href="#fasilitas">Fasilitas</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-light active" href="#pemateri">Pemateri</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-light active" href="#register">Daftar</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <div id="home" class="parallax">
+        <div class="container d-flex justify-content-center align-items-center parallax-content" style="height:100vh;">
+            <div class="col-12 col-md-10 col-lg-8 d-flex justify-content-center flex-column">
+                <h1>Code Conqueror</h1>
+                <p>Selain Kita Jago Ngode, Kita Juga Harus Jago Ngoding</p>
+                <div class="row">
+                    <div class="col d-flex justify-content-center"><a href="#register" class="btn btn-primary" type="button">Daftar</a></div>
+                    
+                </div>
+            </div>
+        </div><video class="parallax-background" autoplay="" loop="" muted=""><source src="{{ asset('alghiffaryenterprise/video.mp4')}}" type="video/mp4" wp-acf="[{'type':'url','name':'video','label':'Video','wrapper':{'width':25}},{'type':'text','name':'video_css','label':'Video CSS (eg. filters)','wrapper':{'width':25}}]" wp-attr="[{'target':'src','replace':'%1'},{'target':'parent_style','replace':'%2'}]"></video>
+        <div
+            class="parallax-placeholder" style="background-image:url(&quot;assets/img/placeholder.jpg&quot;);"></div>
+    </div>
+    <div id="lb" style="text-align:center;">
+        <h2 class="divider-style"><span>Latar Belakang</span></h2>
+    </div>
+    <div class="dh-highlight-text">
+        <div class="container">
+            <p>Zaman sekarang selain kita harus tau Bahasa Inggris, kita juga harus tahu Bahasa Pemrograman, karena untuk menunjang kita berpartisipasi dalam Industry Revolution 4.0 & Society 5.0 serta Indonesia Emas 2045 &nbsp; </p>
+        </div>
+    </div>
+    <div id="time" style="text-align:center;">
+        <h2 class="divider-style"><span>Waktu</span></h2>
+    </div><div class="card-deck-wrapper">
+  <div class="card-deck">
+  <div class="row">
+			 
+             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                 <i class="material-icons" style="font-size:48px;">calendar_today</i>
+                     
+                     <div class="title">
+                         <h4>Tanggal</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>14-16 Oktober 2019</span>
+                     </div>
+                     
+                     
+                     
+                  </div>
+             </div>	 
+             
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                 <i class="material-icons" style="font-size:48px;">schedule</i>
+                 
+                     <div class="title">
+                         <h4>Jam</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>08.00 - 12.00</span>
+                     </div>
+                     
+              
+                     
+                  </div>
+             </div>	 
+             
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                 <i class="material-icons" style="font-size:48px;">near_me</i>
+                     
+                     <div class="title">
+                         <h4>Battleground</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Al Ghiffary Enterprise Building 14th Floor</span>
+                     </div>
+                     
+                     
+                     
+                  </div>
+             </div>	 
+     
+     </div>
+  </div>
+</div>
+    <div id="fasilitas" style="text-align:center;">
+        <h2 class="divider-style"><span>Fasilitas</span></h2>
+    </div>
+    <div>
+        <div class="container">
+            <div class="cust_bloglistintro">
+                <p class="text-muted text-center">Jika Kamu Mengikuti Program Ini, Kamu Akan Mendapat&nbsp; </p>
+            </div>
+            <div class="row">
+                <div class="col-md-4 cust_blogteaser"><a href="#"><img style="object-fit: cover" width="400px" height="270px" src="{{ asset('alghiffaryenterprise/img/sertif.jpg')}}"></a>
+                    <h3>Ilmu yang Bermanfaat dan Sertifikat</h3>
+                    </div>
+                    <div class="col-md-4 cust_blogteaser"><a href="#"><img style="object-fit: cover" width="400px" height="270px" src="{{ asset('alghiffaryenterprise/img/relasi.jpeg')}}"></a>
+                    <h3>Teman dan Relasi</h3>
+                    </div>
+                    <div class="col-md-4 cust_blogteaser"><a href="#"><img style="object-fit: cover" width="400px" height="270px" src="{{ asset('alghiffaryenterprise/img/makan.jpg')}}"></a>
+                    <h3>Snack dan Makan Siang</h3>
+                    </div>
+                    </div>
+    </div>
+    </div>
+    <div id="pemateri" style="text-align:center;">
+        <h2 class="divider-style"><span>Pemateri</span></h2>
+    </div>
+    <div class="blog-slider">
+        <div class="blog-slider__wrp swiper-wrapper">
+            <div class="blog-slider__item swiper-slide">
+                <div></div>
+                <div class="blog-slider__img"><img src="{{ asset('alghiffaryenterprise/img/dicong.jpg')}}"></div>
+                <div class="blog-slider__content"><span class="blog-slider__code">Pemateri</span><div class="blog-slider__title">Narenda Wicaksono</div><div class="blog-slider__text">CEO Dicoding</div></div>
+            </div>
+            <div class="blog-slider__item swiper-slide">
+                <div></div>
+                <div class="blog-slider__img"><img src="{{ asset('alghiffaryenterprise/img/ahmad.jpg')}}"></div>
+                <div class="blog-slider__content"><span class="blog-slider__code">Pemateri</span><div class="blog-slider__title">Ahmad Syarif</div><div class="blog-slider__text">CEO & Founder CyberLabs</div></div>
+            </div>
+            <div class="blog-slider__item swiper-slide">
+                <div></div>
+                <div class="blog-slider__img"><img src="{{ asset('alghiffaryenterprise/img/fauzan.jpg')}}"></div>
+                <div class="blog-slider__content"><span class="blog-slider__code">Moderator</span><div class="blog-slider__title">Fauzan Amir Al Ghiffary</div><div class="blog-slider__text">CEO Alghiffary Enterprise</div></div>
+            </div>
+            <div class="blog-slider__pagination"></div>
+        </div>
+    </div>
+    <div id="register" class="float-none" style="text-align:center;">
+        <h2 class="divider-style"><span>Pendaftaran</span></h2>
+    </div>
+    <div class="jumbotron hero-technology">
+        <div class="row register-form">
+            <div class="col-md-8 offset-md-2">
+                <form class="custom-form">
+                    <div class="form-row form-group">
+                        <div class="col-sm-4 label-column"><label class="col-form-label" for="name-input-field">Nama </label></div>
+                        <div class="col-sm-6 input-column"><input class="form-control" type="text"></div>
+                    </div>
+                    <div class="form-row form-group">
+                        <div class="col-sm-4 label-column"><label class="col-form-label" for="email-input-field">Email </label></div>
+                        <div class="col-sm-6 input-column"><input class="form-control" type="email"></div>
+                    </div>
+                    <div class="form-row form-group">
+                    <div class="col-sm-4 label-column"><label class="col-form-label" for="name-input-field">No Whatsapp </label></div>
+                        <div class="col-sm-6 input-column"><input class="form-control" type="text"></div>
+                    </div>
+                 <button class="btn btn-light submit-button" type="button">Daftar Gan</button></form>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 footer-navigation">
+            <img id="brand-logo" src="{{ asset('alghiffaryenterprise/alghiffaryfa.png')}}">
+                <p class="company-name">Copyright Alghiffary Enterprise © 2019 </p>
+            </div>
+            <div class="col-sm-6 col-md-4 footer-contacts">
+                <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p><span class="new-line-span">Sambungan, Jogorogo</span> Ngawi, Jawa Timur</p>
+                </div>
+                <div><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p class="footer-center-info email text-left"> +6281555453315</p>
+                </div>
+                <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                    <p> <a href="#" target="_blank">alghiffaryfa19@gmail.com</a></p>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="col-md-4 footer-about">
+                <h4>Tentang Alghiffary Enterprise</h4>
+                <p> Merupakan sebuah perusahaan yang fokus dalam pengembangan aplikasi, web dan gim, desain grafis dan apparel</p>
+                <div class="social-links social-icons"><a href="https://www.facebook.com/fauzan.amir.94"><i class="fa fa-facebook"></i></a><a href="https://www.instagram.com/fauzanamiralghiffary/?hl=id"><i class="fa fa-instagram"></i></a></div>
+            </div>
+        </div>
+    </footer>
+    <script src="{{ asset('alghiffaryenterprise/js/jquery.min.js')}}"></script>
+    <script src="{{ asset('alghiffaryenterprise/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('alghiffaryenterprise/js/--mp---navbar-shrinking-on-scroll.js')}}"></script>
+    <script src="{{ asset('alghiffaryenterprise/js/Paralax-Hero-Banner.js')}}"></script>
+    <script src="{{ asset('alghiffaryenterprise/js/Swiper-Slider-Card-For-Blog-Or-Product.js')}}"></script>
+    <script src="{{ asset('alghiffaryenterprise/js/Video-Parallax-Background-v2.js')}}"></script>
+</body>
+
+</html>
